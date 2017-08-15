ff2982b3e6eed49f3603427422cd657c33cb2383
xiaoke <ke.xiao@trigtop.com>
2017-08-15 15:35:03 +0800

    default app_name Kodi->Qodi

--- tools/android/packaging/xbmc/src/org/xbmc/kodi/Splash.java
+++ tools/android/packaging/xbmc/src/org/xbmc/kodi/Splash.java
@@ -147,7 +147,7 @@ public class Splash extends Activity {
 
           break;
         case StartingXBMC:
-          mSplash.mTextView.setText("Starting Kodi...");
+          mSplash.mTextView.setText("Starting Qodi...");
           mSplash.mProgress.setVisibility(View.INVISIBLE);
           mSplash.startXBMC();
           break;
